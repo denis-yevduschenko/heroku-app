0e1101ed996964406398bfe4dcd666b481beeb2e
dyevduschenko <dyevduschenko>
2017-02-28 17:06:53 +0200

    add questions to App

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,10 @@
 CofenatorApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.currentZoomItem = '';
     $scope.choosenAdditives = '';
-
+    $scope.filterBy = [];
+    $scope.typeOfCoffee = '';
+    $scope.roast = '';
+    $scope.cupSize = '';
 
     $http.get('/data/coffee.json').then(function(response){
         $scope.coffee = response.data;
@@ -22,13 +25,21 @@ CofenatorApp.controller('MainController', ['$scope', '$http', function($scope, $
     $scope.chooseAdditive = function (additive) {
         var index = $scope.choosenAdditives.indexOf(additive.combined);
         if (index > 0){
+            /*del string from result block*/
             $scope.choosenAdditives = $scope.choosenAdditives.substring(0, index) + $scope.choosenAdditives.substring(index + additive.combined.length);
+            /*del additive from filter array*/
+            arrayIndex = $scope.filterBy.indexOf(additive.combined);
+            if (arrayIndex > -1) {
+                $scope.filterBy.splice(arrayIndex, 1);
+            }
         } else {
+            /*add string to result block*/
             $scope.choosenAdditives += " " + additive.combined;
+            /*add additive to filter array*/
+            $scope.filterBy.push(additive.combined);
         }
     };
-    /*TODO:: Attach filtered array to checkbox */
-    $scope.filterBy = ['Milk', 'Sugar', 'Alcohol'];
+    /*TODO:: reduce counts of additives to 1 in coffee.json*/
     $scope.filteredCoffee = function () {
         return $scope.coffee.filter(function (item) {
             return $scope.filterBy.indexOf(item.combined[0].taste) !== -1;
